b95fe03b823a3052b9827ee2a0da8cbde17d7215
Santosh Yadav <santosh.yadav@coupa.com>
2026-04-05 11:20:03 +0530

    add loop project

--- loop-project/Program.cs
+++ loop-project/Program.cs
@@ -1,15 +1,18 @@
 ﻿Console.Write("User1, Guess the number?: ");
 int exactNumber = int.Parse(Console.ReadLine());
+int guessCount = 0;
 
 Console.WriteLine("It's turn for User 2 to guess.");
 while (true)
 {
   Console.Write("Guess the number, User 2: ");
   int guessedNumber = int.Parse(Console.ReadLine());
+  guessCount++;
 
   if(guessedNumber == exactNumber)
   {
-    Console.WriteLine("Wooooooh!, Congratulation you have guessed the number.");
+    Console.WriteLine("\n\nWooooooh!, Congratulation you have guessed the number.");
+    Console.WriteLine($"You did this in {guessCount} guesses\n");
     break;
   }
 
