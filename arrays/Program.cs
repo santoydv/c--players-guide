3cfb42b54b7281a82d11d5e9c3a34fbb173a5ff3
Santosh Yadav <santosh.yadav@coupa.com>
2026-04-05 20:48:53 +0530

    array understanding

---
+++ arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] scores = new int[5];
+scores = new int[]{1,2,3,4,5};
+
+Console.WriteLine(scores[0]);
+Console.WriteLine(scores[^1]);
+
+Console.Write("The scores are: ");
+foreach(int score in scores)
+{
+  Console.Write($"{score} ");
+}
+Console.WriteLine();
+
+int Double(int value) => 2 * value;
+
+Console.WriteLine(Double(4));
